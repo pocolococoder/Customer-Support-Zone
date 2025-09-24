829d7572a05047a79f33ca0b5039270c4106d063
rez1-dev <rezwan.mahmud.007@gmail.com>
2025-09-24 22:44:01 +0600

    added Navbar and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-     <h1>Hi!</h1>
+     <Navbar></Navbar>
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='bg-white-200 shadow-2xl border-b-[0.5px] border-gray-200 h-[50px] '>
+         
+                <nav className='max-w-[1200px] mx-auto'>
+                    <h1 className='md:text-xl text-sm font-bold'>CS — Ticket System</h1>
+                </nav>
             
         </div>
     );
